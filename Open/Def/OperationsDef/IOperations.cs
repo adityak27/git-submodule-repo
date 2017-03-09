170d3540d3761933c7682b49b9a2a598105be508
adityk <adityk@ADITYA-PC1>
2017-03-08 19:11:19 -0800

    First commit to git submodule

---
+++ Open/Def/OperationsDef/IOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsDef
+{
+    public interface IOperations
+    {
+        int Add(int val1, int val2);
+
+        int Subtract(int val1, int val2);
+
+        int Multiply(int val1, int val2);
+
+        int Divide(int val1, int val2);
+    }
+
+    public enum OperationType
+    {
+        Add,
+        Subtract,
+        Divide,
+        Multiply
+    }
+}
---
+++ Open/Impl/OperationsImpl/Operations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OperationsDef;
+
+namespace OperationsImpl
+{
+    public class Operations : IOperations
+    {
+        Logger.Logger logger;
+
+        public Operations()
+        {
+            logger = Logger.Logger.GetLogger(@"C:\OperationsLog.txt");
+        }
+
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        public int Divide(int val1, int val2)
+        {
+            return val1 / val2;
+        }
+
+        public int Multiply(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+
+        public int Subtract(int val1, int val2)
+        {
+            return val1 - val2;
+        }
+    }
+}
